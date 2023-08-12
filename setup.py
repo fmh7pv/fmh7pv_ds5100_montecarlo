49e4e059e6a0126af7f09c4603dd089ad501646a
fmh7pv <104300517+fmh7pv@users.noreply.github.com>
2023-08-11 22:11:02 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='montecarlo',
+      version='0.1',
+      description='A package made to implement a simple Monte Carlo simulator using a set of three related classes — a Die class, a Game class, and an Analyzer class.',
+      url='https://github.com/fmh7pv/fmh7pv_ds5100_montecarlo.git',
+      author='Fadumo Hussein',
+      author_email='fmh7pv@virginia.edu',
+      license='MIT',
+      packages=['fmh7pv_ds5100_montecarlo'])
